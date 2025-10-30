61f51cb7c8be7da80642d1025a9ed780aa78707d
Shahbaz khan <shazzykhan750@gmail.com>
2025-10-31 00:18:32 +0530

    firebase commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -11,13 +11,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAOVvWVZcDy58U94j_FaKOR_Q96NcEOKuA",
-  authDomain: "tmdb-6e244.firebaseapp.com",
-  projectId: "tmdb-6e244",
-  storageBucket: "tmdb-6e244.firebasestorage.app",
-  messagingSenderId: "580615161524",
-  appId: "1:580615161524:web:cc047d5d6e9246ed2e39e1",
-  measurementId: "G-2EZBPHW0VW",
+  apiKey: "AIzaSyBDkIi0NBj6WnxhUTFuV2TZ8PLnZohwbUM",
+  authDomain: "tmdbproject-b23f3.firebaseapp.com",
+  projectId: "tmdbproject-b23f3",
+  storageBucket: "tmdbproject-b23f3.firebasestorage.app",
+  messagingSenderId: "251288860006",
+  appId: "1:251288860006:web:5b8a4cb549a699c7ad04a7",
+  measurementId: "G-F39EMC1TH5",
 };
 
 // Initialize Firebase
